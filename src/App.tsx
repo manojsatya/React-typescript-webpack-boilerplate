a19644e49c7a09088d3ed83dc053f371348eebfa
manojsatya <manoj.pesit@gmail.com>
2021-03-29 23:07:56 +0200

    Added css, png and svg files

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
+import './styles.css'
+import IMAGE from './react-logo.png'
+import LOGO from './apple-logo.svg'
+
 export const App = () => {
-    return <h1>React Typescript Webpack Starter Template</h1>
+    return (
+    <>
+    
+    <h1>React Typescript Webpack Starter Template {process.env.NODE_ENV} - {process.env.name}</h1>
+
+    <img src={IMAGE} alt="logo" width="200" height="200"   />
+    <img src={LOGO} alt="logo" width="200" height="200"   />
+    </>
+    )
 }
