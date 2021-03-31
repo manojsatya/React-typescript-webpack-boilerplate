cf96b54bfddd8747ce80095a74263df9d6ebe552
manojsatya <manoj.pesit@gmail.com>
2021-03-31 23:35:57 +0200

    Added prettier and eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import IMAGE from './react-logo.png'
 import LOGO from './apple-logo.svg'
 
 export const App = () => {
-    return (
+  return (
     <>
-    
-    <h1>React Typescript Webpack Starter Template {process.env.NODE_ENV} - {process.env.name}</h1>
+      <h1>
+        React Typescript Webpack Starter Template {process.env.NODE_ENV} -{' '}
+        {process.env.name}
+      </h1>
 
-    <img src={IMAGE} alt="logo" width="200" height="200"   />
-    <img src={LOGO} alt="logo" width="200" height="200"   />
+      <img src={IMAGE} alt="logo" width="200" height="200" />
+      <img src={LOGO} alt="logo" width="200" height="200" />
     </>
-    )
+  )
 }
