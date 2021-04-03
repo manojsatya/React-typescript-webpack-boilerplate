bf3dfb4838b2cf3a4067820d2f03b599f24ca5f3
manojsatya <manoj.pesit@gmail.com>
2021-04-03 23:20:40 +0200

    Added extra packages in webpack

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
@@ -28,7 +28,7 @@ module.exports = {
       {
         test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
         type: 'asset/inline',
-      }
+      },
     ],
   },
   output: {
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,12 +1,16 @@
 const webpack = require('webpack')
-
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+  .BundleAnalyzerPlugin
 
 module.exports = {
-    mode: 'production',
-    devtool: 'source-map', // controls generation of source maps
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.name': JSON.stringify('Manoj production') // Used for API endpoints
-        })
-    ]
+  mode: 'production',
+  devtool: 'source-map', // controls generation of source maps
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.name': JSON.stringify('Manoj production'), // Used for API endpoints
+    }),
+    new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }),
+  ],
 }
